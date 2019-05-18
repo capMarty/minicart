ec61263ccb261e6ed1b66d8aa399ab3afe525cbc
capMarty <49485940+capMarty@users.noreply.github.com>
2019-05-18 10:16:14 +0300

    Update classItem.php

--- тестовое задание/classItem.php
+++ тестовое задание/classItem.php
@@ -1,84 +1,109 @@
 <?php 
 
-class item{
+final class item{
 
-	private $id;
-	private $name;
-	private $status;
-	private $changed;
-
-
-	public function __construct($id)
-	{
-		$this->id = (int)$id; 
-	}
-
-	private function init()
-	{
-
-		// одноразовый вызов метода 
-		static $isCallInit = false;
-		$this->isCallInit = $isCallInit;
-
-		if (!$isCallInit) {
- 			// получаем данные из БД, где в
- 			// $nameFromDB - находится результат запроса поля name
- 			// $statusFromDB- находится результат запроса поля status
-			$this->name = (string)$nameFromDB;
-			$this->status = (int)$statusFromDB;
-			
+    private $id;
+    private $name;
+    private $status;
+    private $changed;
+    // create a variable to determine the initialization of the function call
+    private $isCallInit = false ;
+    // create a variable to store values from DB
+    private $getDataDB;
 
 
+    /**
+     * item constructor.
+     * @param $id
+     */
+    public function __construct($id)
+    {
+        $this->id = (int)$id;
+        $this->init();
+    }
 
-			$isCallInit = true;	
-		}
-	}
+    /**
+     *
+     */
+    private function init()
+    {
 
-	public function __get($property)
-	{
-		if (property_exists($this, $property)) {
+        //one-time method call
 
-			$this->init();
-			echo $this->$property;
+        $isCallInit = $this->isCallInit;
+        if (!$isCallInit) {
 
-		}	
+            //get data from db where in
+            //$ nameFromDB - is the result of the query field name
+            //$ statusFromDB - is the result of the query field status
+            $nameFromDB = "данные из DB";
+            $statusFromDB = 11964;
 
-	}
+            //create an array for storage
+            $getDataDB['nameFromDB'] = $nameFromDB;
+            $getDataDB['statusFromDB'] = $statusFromDB;
+            
+		$this->name = (string)$nameFromDB;
+            $this->status = (int)$statusFromDB;
+            $this->isCallInit = true;
+            //put the data in the $getDataDB variable
+            $this->getDataDB = $getDataDB;
 
-	public function __set($property, $value)
-	{
-		if (property_exists($this, $property)) {
-
-			$this->init();
-			// проверка вводимого значения на заполненность и тип значения
-			if ( trim($value) ) 
-
-				switch ($property) {
-					case 'name':
-						$this->$property = (string)$value;
-					break;
-					case 'status':
-						$this->$property = (int)$value;
-					break;
-				}
-
-			}
+        }
 
 
-	}
-	// Метод сохраняет установленные значения name и status в случае, если свойства объекта были изменены извне
-	public function save()
-	{
-			$this->init();
-			// сравниваем значение
-			if ($nameFromDB != $this->name)
-				 // если данные были изменены, обновляем, и делаем UPDATE в БД
-				$nameFromDB = $this->name;
 
-			if ($statusFromDB != $this->status) 
-				$statusFromDB = $this->status;
-			
-	}
+    }
+
+
+    /**
+     * @param $property
+     * @return mixed
+     */
+    public function __get($property)
+    {  
+        if (property_exists($this, $property)) {
+//
+            return $this->$property;
+
+        }
+    }
+
+    public function __set($property, $value)
+    {
+        if (property_exists($this, $property)) {
+
+            //check of the entered value for fullness and type of value
+            if ($value != '')
+
+                if (is_string($value) && $property === 'name')
+                    $this->$property = (string)$value;
+
+                if (is_int($value) && $property === 'status')
+                $this->$property = (int)$value;
+
+
+        }
+    }
+
+	 //The method saves the specified NAME and STATUS values in case the object properties have been changed externally.
+    public function save()
+    {
+            $getDataDB = $this->getDataDB;
+
+            //compare values
+		if ($getDataDB['nameFromDB'] != $this->name)
+            
+            		//if the data has been changed, do UPDATE in the database
+            		$getDataDB['nameFromDB'] = $this->name;
+
+        	if ($getDataDB['statusFromDB'] != $this->status)
+            
+            		//if the data has been changed, do UPDATE in the database
+           		 $getDataDB['statusFromDB'] = $this->status;
+    }
+
+
 }
 
 	
